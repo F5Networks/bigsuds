27b4c22b202925702521493137b422512cf568a9
Tim Rupp <caphrim007@gmail.com>
2017-10-25 11:48:02 -0700

    added travis file and removed old test file

--- test.py
+++
@@ -1,8 +0,0 @@
-import bigsuds
-
-api = bigsuds.BIGIP(hostname='localhost', username='admin', password='admin', verify=False, port=10443)
-result = api.System.SystemInfo.get_uptime()
-print(result)
-
-result = api.System.SystemInfo.get_product_information()
-print(result)
